337302eab86e00efa67107b4a4c918e9cf4dec71
Rahul Patel <rahul.patel.28830@gmail.com>
2025-04-16 18:30:51 -0700

    Time: 4 ms (59.46%) | Memory: 15.6 MB (10.47%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,23 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> solution;
+        std::unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (map[target - nums[i]] >= 0) {
+                if ((nums[i] + nums[map[target - nums[i]]] == target) && i != 0) {
+                    solution.push_back(map[target - nums[i]]);
+                    solution.push_back(i);
+                    return solution;
+                } else {
+                    map[nums[i]] = i;
+                }
+            }
+        }
+
+        return solution;
+    }
+};
