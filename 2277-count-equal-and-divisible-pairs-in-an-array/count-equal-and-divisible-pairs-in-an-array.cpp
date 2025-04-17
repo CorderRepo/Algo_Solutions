e12750ee4ab6f74854a729f6ca120a2e119ce5e8
Rahul Patel <rahul.patel.28830@gmail.com>
2025-04-16 19:16:42 -0700

    Time: 0 ms (100.00%) | Memory: 15.5 MB (64.71%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -3,6 +3,10 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
+
+        /*
+        Attempt 1, Good for large inputs
+
         // Hash Map for value lookup, Keys hold the value in the array and stored value is a vector of indexes
         std::unordered_map<int, vector<int>> map;
         int answer = 0;
@@ -20,6 +24,19 @@ public:
             } else {
                 // Add current index to the index vector in the hash map for key nums[i]
                 map[nums[i]].push_back(i);
+            }
+        }
+
+        return answer;
+        */ 
+
+        // Attempt 2, Good for low to medium inputs
+        int answer = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] == nums[j] && (i*j) % k == 0) {
+                    answer++;
+                }
             }
         }
 
