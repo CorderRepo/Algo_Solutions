13256e59b913e4ea0df2179a7173c48375706c87
Rahul Patel <rahul.patel.28830@gmail.com>
2025-05-25 00:00:09 -0700

    Time: 15 ms (73.01%) | Memory: 25 MB (67.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,21 +1,21 @@
 #include <unordered_map>
-using namespace std;
+#include <vector>
+#include <algorithm>
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mapChecker;
+        std::unordered_map<string, vector<string>> log;
+        std::vector<vector<string>> solution = {};
 
-        for (auto & str : strs) {
+        for (auto &str : strs) {
             string temp = str;
-            sort(str.begin(),str.end());
-            mapChecker[str].emplace_back(move(temp));
+            std::sort(str.begin(), str.end());
+            log[str].emplace_back(std::move(temp));
         }
 
-        vector<vector<string>> solution = {};
-
-        for (auto & [_,sub] : mapChecker) {
-            solution.emplace_back(move(sub));
+        for (auto [_,sub] : log) {
+            solution.emplace_back(std::move(sub));
         }
 
         return solution;
